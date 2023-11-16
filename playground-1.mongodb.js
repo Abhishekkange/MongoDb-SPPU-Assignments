1af6f2d9d800f0a1735d0ad61e6c66dda9522314
Abhishek <Abhishekkange@gmail.com>
2023-11-16 22:06:45 +0530

    almost completed Assignment 1

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -30,4 +30,57 @@ db.employee.insertOne({ "eid": 10, "ename": "Ava Davis", "designation": "IT Proj
 
 
 //1. List the Names of Analysts and Salesman
-db.employee.find({designation:{$in:['web Developer','IT Consultant']}});
+db.employee.find({designation:{$in:['web Developer','IT Consultant']}},{ename:1,_id:0});
+
+//2. List the eid,ename and salary from employee collection.
+db.employee.find({},{ename:1,eid:1,salary:1,_id:0});
+
+//3.List names of employees who are not managers.
+db.employee.find({"designation":{$ne:"Web Developer"}},{ename:1,_id:0});
+
+//4. List the names of employees whose employee numbers are 1,3,7,9.
+db.employee.find({eid:{$in:[1,3,7,9]}},{ename:1,_id:0});
+
+//5.List the names of all employees those having reading as a secound hobby.
+
+//6.who have joined between 30 June and 31 Dec 2015.
+    // use $gte for greater than and $lte for less than
+    db.employee.find({"hiredate":{$gte:ISODate("2015-06-01"),$lte:ISODate("2015-06-05")}});
+
+//7. List the different designations in the company.
+db.employee.distinct("designation");
+
+//8.List the eid,ename,salary of all employees whose salary is less than 10000.
+db.employee.find({"salary":{$lte:1000}},{ename:1,eid:1,salary:1,_id:0});
+
+//9.List the name and designation of the employee who works in production department.
+db.employee.find({"department":{$eq:"IT"}},{ename:1,designation:1,_id:0});
+
+//10.List the all employees whose name start with "A" letter.
+//Study about using Regular Expression in MongoDB
+
+//14. List the number of employees working in sales department.
+db.employee.countDocuments();
+
+//15.List the number of designations available in the EMP collections.
+db.employee.countDocuments({"designation":"Web Developer"});
+
+// 16.  List the eid,ename,salary of all employees whose salary in between 10000 to 20000.
+
+db.employee.find({"salary":{$gt:1000,$lt:20000}},{eid:1,ename:1,salary:1,_id:0});
+
+//17. List the eid,ename of all employees whose salary is gretter than or equal to 15000.
+db.employee.find({"salary":{$gte:15000}},{ename:1,salary:1,eid:1,_id:0});
+
+//18.List details of employees whose department is Sales and salary is 10000.
+db.employee.find({"department":"IT","salary":38000},{ename:1,_id:0});
+
+
+
+//19. List the names of employees those having reading and painting hobbies.
+db.employee.find({"hobbies":{$in:['reading','painting']}});
+
+//20.List the first hobby of all employees from the employee collection.
+
+//21. List the names of all employees those having three different hobbies.
+
